ca0a3c670fb91e821981e60cc4efdabf535ae90f
OKASHA MAAZ <maazokasha0@gmail.com>
2026-09-03 00:47:25 +0530

    Time: 3 ms (29.65%), Space: 7.9 MB (23.17%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        long long  i = 0;
+        long long j = (int)sqrt(c);
+        while(i<=j){
+            if((i*i+j*j) == c){
+                return true;
+            }
+            else if((i*i+j*j) > c){
+                j--;
+            }
+            else{
+                i++;
+            }
+     
+        }
+    return false;
+    }
+   
+};
