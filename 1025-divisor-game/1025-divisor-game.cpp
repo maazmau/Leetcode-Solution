ab0ed684053b877c37538da65f99504c5bf62195
OKASHA MAAZ <maazokasha0@gmail.com>
2026-09-06 01:14:29 +0530

    Time: 0 ms (100%), Space: 7.7 MB (57.77%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool divisorGame(int n) {
+        int x = 2;
+        if(n % x == 0){
+            return true;
+        }
+        else{
+        return false;
+        }
+    }
+};
